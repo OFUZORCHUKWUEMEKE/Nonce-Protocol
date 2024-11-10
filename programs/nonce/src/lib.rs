a3fc9e43c660070639fe9cfba674d1d77b333fb7
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-11-10 13:27:12 +0000

    folder structure

--- programs/nonce/src/lib.rs
+++ programs/nonce/src/lib.rs
@@ -5,12 +5,6 @@ declare_id!("4XSnD5eXrLPEuvEmN9hieF3ywBKXKPU2hogH7fUKYxmF");
 #[program]
 pub mod nonce {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+    
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
