c25450e81c26030fa17ffca133d05a645a19d04d
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-11-10 19:55:50 +0000

    initialize refactor

--- programs/nonce/src/instructions/initialize.rs
+++ programs/nonce/src/instructions/initialize.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
-use crate::state::{SavingsAccount};
+use crate::state::{SavingsAccount,SavingsType};
 
 #[derive(Accounts)]
-#[instructions(name:String,description:String,savings_type:SavingsType)]
+#[instructions(name:String,description:String,savings_type:SavingsType,is_sol:bool)]
 pub struct InitializeSavings<'info>{
     #[account(mut)]
     pub signer:Signer<'info>,
@@ -14,7 +14,17 @@ pub struct InitializeSavings<'info>{
         space=SavingsAccount::INIT_SPACE
     )]
     pub savings_account:Account<'info,SavingsAccount>,
-    pub usdc_mint:InterfaceAccount<'info,token_interface::Mint>,
+    #[account(
+        mut,
+        constraint = if !is_sol{
+            let ata = get_associated_token_address(&owner.key(),&usdc_mint.as_ref().unwrap().key());
+            token_account.as_ref().unwrap().key()
+        }else{
+            true
+        }
+    )]
+    pub token_account:Option<InterfaceAccount<'nfo,token_program::TokenAccount>>
+    pub usdc_mint:Option<InterfaceAccount<'info,token_interface::Mint>>,
     pub token_program:InterfaceAccount<'info,token_interface::TokenInterface>,
 
 }
