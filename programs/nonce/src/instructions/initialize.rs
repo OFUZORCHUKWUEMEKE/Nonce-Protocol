ac6457b697754af92460c8ca46634df651f95133
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-11-11 09:04:54 +0000

    initialize

--- programs/nonce/src/instructions/initialize.rs
+++ programs/nonce/src/instructions/initialize.rs
@@ -1,11 +1,12 @@
+use crate::state::{SavingsAccount, SavingsType};
 use anchor_lang::prelude::*;
-use crate::state::{SavingsAccount,SavingsType};
+use anchor_spl::associated_token::get_associated_token_address;
 
 #[derive(Accounts)]
 #[instructions(name:String,description:String,savings_type:SavingsType,is_sol:bool)]
-pub struct InitializeSavings<'info>{
+pub struct InitializeSavings<'info> {
     #[account(mut)]
-    pub signer:Signer<'info>,
+    pub signer: Signer<'info>,
     #[account(
         init,
         seeds=[name.as_bytes().as_ref(),signer.key().as_ref(),description.as_ref(),savings_type.to_bytes()],
@@ -13,7 +14,7 @@ pub struct InitializeSavings<'info>{
         payer=signer,
         space=SavingsAccount::INIT_SPACE
     )]
-    pub savings_account:Account<'info,SavingsAccount>,
+    pub savings_account: Account<'info, SavingsAccount>,
     #[account(
         mut,
         constraint = if !is_sol{
@@ -23,8 +24,30 @@ pub struct InitializeSavings<'info>{
             true
         }
     )]
-    pub token_account:Option<InterfaceAccount<'nfo,token_program::TokenAccount>>
-    pub usdc_mint:Option<InterfaceAccount<'info,token_interface::Mint>>,
-    pub token_program:InterfaceAccount<'info,token_interface::TokenInterface>,
+    pub token_account: Option<InterfaceAccount<'nfo, token_program::TokenAccount>>,
+    pub usdc_mint: Option<InterfaceAccount<'info, token_interface::Mint>>,
+    pub token_program: InterfaceAccount<'info, token_interface::TokenInterface>,
+    pub system_program: Progrm<'info, System>,
+}
 
+pub fn initialize(
+    ctx: Context<InitializeSavings>,
+    name: String,
+    description: String,
+    savings_type: SavingsType,
+    is_sol: bool,
+    sol_amount: Option<u64>,
+    usdc_amount: Option<i64>,
+) -> Result<()> {
+    let savings_account = &mut ctx.accounts.savings_account;
+    savings_account.name = name;
+    savings_account.description = description;
+    savings_account.savings_type = savings_type;
+    savings_account.is_sol = is_sol;
+    savings_account.owner = ctx.accounts.owner.key();
+    savings_account.bump = ctx.bumps.accounts.savings_account;
+    savings_account.created_at = Clock::get()?;
+    savings_account.sol_amount = sol_amount;
+    savings_account.usdc_amount = usdc_amount;
+    savings_account.bump = ctx.bumps.get("savings_account").unwrap()
 }
--- programs/nonce/src/state.rs
+++ programs/nonce/src/state.rs
@@ -1,6 +1,5 @@
 use anchor_lang::prelude::*;
 
-
 #[account]
 #[derive(InitSpace)]
 pub struct SavingsAccount{
@@ -10,17 +9,18 @@ pub struct SavingsAccount{
     pub usdc_amount:Option<f64>,
     #[max_len(50)]
     pub description:String,
-    pub owner:PubKey,
+    pub owner:Pubkey,
     pub bump:u8,
     pub is_active:bool,
-    pub lock_duration:164,
+    pub lock_duration:Option<i64>,
     pub created_at:i64,
     pub savings_type:SavingsType,
     pub total_sol_saved:u64,
     pub total_usdc_saved:u64
 }
 
-#[derive(AnchorDeserialize,AnchorSerialize,PartialEq,Eq)]
+#[derive(AnchorDeserialize,AnchorSerialize,PartialEq,Eq,Clone)]
+#[derive(InitSpace)]
 pub enum SavingsType{
     TimeLockedSavings,
     PriceLockedSavings
